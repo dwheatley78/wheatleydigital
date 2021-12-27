ec04ba2c46808ed98e23781943a1b9a607c14b14
David <dwheatley@live.co.uk>
2021-12-27 15:19:41 +0000

    Added new snake game

---
+++ henry/snake/constants.js
@@ -0,0 +1,8 @@
+const board_border = 'black';
+const board_background = "yellow";
+const snake_col = 'lightblue';
+const snake_border = 'darkblue';
+
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
