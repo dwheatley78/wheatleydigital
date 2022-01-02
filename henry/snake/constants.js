9e758e904c6a7bcab5cd069752b7c5180fbd99a8
David <dwheatley@live.co.uk>
2022-01-02 21:40:14 +0000

    added angry birds

--- henry/snake/constants.js
+++
@@ -1,8 +0,0 @@
-const board_border = 'black';
-const board_background = "yellow";
-const snake_col = 'lightblue';
-const snake_border = 'darkblue';
-
-const COLS = 10;
-const ROWS = 20;
-const BLOCK_SIZE = 30;
